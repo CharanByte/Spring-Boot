fe695f360f715c8a7a2ad6ff98d1fafde8a7bfdb
CharanByte <charan7812@gmail.com>
2025-03-05 22:32:20 +0530

    performing CRUD operations using JPA in Spring Boot

---
+++ demo-gradle/src/main/java/com/example/demo_gradle/DemoGradleApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class DemoGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGradleApplication.class, args);
+	}
+
+}
